063838d6a0c594c324bfd991fbc9d2af4fae34a8
Diketso Setho <45754364+Diketsosetho16@users.noreply.github.com>
2021-07-29 00:03:49 +0200

    complete project

--- js.js
+++ js.js
@@ -1,21 +1,60 @@
 document.getElementById("start").addEventListener("click", startTimer);
+document.getElementById("cancel").addEventListener("click", cancelPressed);
 
 var startTime;
 var endTime;
 var origTimePeriod;
 var newTimePeriod;
+var timer;
+var cancel = false;
 
 function startTimer(){
+    cancel = false;
     startTime = new Date();
     endTime = getEndTime();
 
-    origTimePeriod = endTime.getTime() - startTime.getTime()
+    origTimePeriod = endTime.getTime() - startTime.getTime();
+    newTimePeriod = origTimePeriod;
 
     timer = setInterval(displayCountdown, 1000);
 }
 
 function displayCountdown(){
-    console.log("HELLO WORLD")
+    var timeLeft = msToTime(newTimePeriod);
+
+   document.getElementById("clock").innerHTML = `<p>${timeLeft["hours"]}h ${timeLeft["mins"]}m ${timeLeft["sec"]}s </p>`;
+   
+   var newHeight = Math.floor(newTimePeriod / origTimePeriod * 100) * 3;
+   document.getElementById("progressBar").setAttribute("height", newHeight);
+
+   newTimePeriod -= 1000;
+
+   if(newTimePeriod < 0 || cancel ){
+       clearInterval(timer)
+       document.getElementById("clock").innerHTML = "<p>Complete</p>";
+   }
+}
+
+function cancelPressed(){
+    cancel = true;
+ 
+}
+
+function msToTime(milliseconds){
+    var mHour = 1000 * 60 * 60;
+    var mMins = 1000 * 60;
+    var mSec = 1000;
+
+    var hours  = Math.floor(milliseconds / mHour);
+    var mins = Math.floor((milliseconds % mHour) / mMins);
+    var sec = Math.floor((milliseconds % mMins) / mSec);
+
+    var time = [];
+    time["hours"] = hours;
+    time["mins"] = mins;
+    time["sec"] = sec;
+
+    return time;
 }
 
 function getEndTime(){
